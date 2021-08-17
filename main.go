4418d874fccd2464c7c644b30021b6002f7875ca
Aleksei Novikov <novikov.aleksei.92@gmail.com>
2021-08-17 19:15:59 +0300

    Added hello world example

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
+import (
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
+)
+
 func Sum(a int, b int) int {
 	return a + b
 }
 
 func main() {
+	app := app.New()
 
+	w := app.NewWindow("Hello")
+	w.SetContent(widget.NewLabel("Hello Fyne!"))
+
+	w.ShowAndRun()
 }
